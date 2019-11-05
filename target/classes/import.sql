876ea244cea17fa2556b04d0b0cf2e83d6de4887
Diogo <diogoalves94@live.com>
2019-11-05 11:17:20 -0300

    Import again

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,5 +1,5 @@
-insert into cliente (id, nome, cpf, telefone, email) values (1, 'Pedro Albuquerque', '22222222222', 'pedraobolado99@hotmail.com');
-insert into cliente (id, nome, cpf, telefone, email) values (2, 'Maria Souza', '33333333333', 'mariasouza38@bol.com');
+insert into cliente (id, nome, cpf, telefone, email) values (1, 'Pedro Albuquerque', '22222222222', '2220252585', 'pedraobolado99@hotmail.com');
+insert into cliente (id, nome, cpf, telefone, email) values (2, 'Maria Souza', '33333333333', '22998588547', 'mariasouza38@bol.com');
 insert into animal (id, nome, raca, especie, sexo, id_proprietario) values (1, 'Bartolomeu', 'Pitbull','Cachorro','m',1);
 insert into animal (id, nome, raca, especie, sexo, id_proprietario) values (2, 'Pé de Pano', 'Bronco','Cavalo','m',2);
 insert into animal (id, nome, raca, especie, sexo, id_proprietario) values (3, 'Thor', 'Realeza','Gato','m',2);
