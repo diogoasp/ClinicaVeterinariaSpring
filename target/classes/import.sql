185064448f82048f567310e563e453281b8acbb9
Diogo <diogoalves94@live.com>
2019-11-05 11:15:29 -0300

    Fiz import

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,6 +1,6 @@
+insert into cliente (id, nome, cpf, telefone, email) values (1, 'Pedro Albuquerque', '22222222222', 'pedraobolado99@hotmail.com');
+insert into cliente (id, nome, cpf, telefone, email) values (2, 'Maria Souza', '33333333333', 'mariasouza38@bol.com');
 insert into animal (id, nome, raca, especie, sexo, id_proprietario) values (1, 'Bartolomeu', 'Pitbull','Cachorro','m',1);
 insert into animal (id, nome, raca, especie, sexo, id_proprietario) values (2, 'Pé de Pano', 'Bronco','Cavalo','m',2);
 insert into animal (id, nome, raca, especie, sexo, id_proprietario) values (3, 'Thor', 'Realeza','Gato','m',2);
-insert into cliente (id, nome, cpf, telefone, email) values (1, 'Pedro Albuquerque', '22222222222', 'pedraobolado99@hotmail.com');
-insert into cliente (id, nome, cpf, telefone, email) values (2, 'Maria Souza', '33333333333', 'mariasouza38@bol.com');
 insert into consulta (id, nome_veterinario, sintomas, diagnostico, dt_consulta, id_animal) values (1, 'Dr. Camilo Pereira', 'Diarréia colossal', 'Virose', '26/12/2017',1);
